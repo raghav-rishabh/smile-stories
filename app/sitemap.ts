7c1f8ecd3d46313ab700a8be6a24df4d1890f9cc
Rishabh Raghav <raghavrishabh99@gmail.com>
2026-05-10 22:36:01 +0530

    dynamic sitemap from sanity

---
+++ app/sitemap.ts
@@ -0,0 +1,29 @@
+import { MetadataRoute } from 'next'
+import { client } from '@/lib/sanity'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await client.fetch(`*[_type == "post"]{ slug, _updatedAt }`)
+
+  const blogUrls = posts.map((post: any) => ({
+    url: `https://www.smilestoriesind.com/blog/${post.slug.current}`,
+    lastModified: new Date(post._updatedAt),
+    changeFrequency: 'yearly' as const,
+    priority: 0.6,
+  }))
+
+  return [
+    {
+      url: 'https://www.smilestoriesind.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1.0,
+    },
+    {
+      url: 'https://www.smilestoriesind.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    ...blogUrls,
+  ]
+}
