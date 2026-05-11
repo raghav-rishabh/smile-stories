deac8e045632eef28d27c012f596dc62018179f9
Rishabh Raghav <raghavrishabh99@gmail.com>
2026-05-11 13:05:50 +0530

    made some changes in page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import { redirect } from "next/navigation"
+// import { redirect } from "next/navigation"
+
+// export default function Home() {
+//   redirect("/index.html")
+// }
 
 export default function Home() {
-  redirect("/index.html")
+  return null // or just an empty component
 }
